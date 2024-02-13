ef3999ff4467674e71660acaf240ce3ccc869f7a
TeddyHuang-00 <huang_nan_2019@pku.edu.cn>
2024-02-13 11:51:54 -0500

    chore: :heavy_plus_sign: Add Nuxt modules and devDependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    "@nuxt/ui",
+    "@nuxtjs/tailwindcss",
+    "@tresjs/nuxt",
+    "@vueuse/nuxt",
+    "nuxt-icon",
+  ],
 });
